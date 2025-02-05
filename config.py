c78a88ee85dd3dd60fe174ff6bce5ecce1e18997
Yana0305 <yanabaranenkova@gmail.com>
2025-02-05 22:43:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = "7899886521:AAGYZpneACtruQeAWApP0FV2xZbiu6KHjG0"  # токен бота
 CHANNEL_URL = "https://t.me/MINESQWERBOT"  # Ссылка на канал
 CHANNEL_ID = "-1002401121555"  # id канала. Начинаеться с минуса
-ADMIN_ID = 1041946577 #tg id твой
+ADMIN_ID = "1041946577" #tg id твой
 
 REF_URL = "http://1wqjnb.com/"  # Ссылка на регистрацию
